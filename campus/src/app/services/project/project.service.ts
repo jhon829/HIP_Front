e7521a89e45f67e37723f8b48ac10986e63e3822
jhk <qwertyjhk11@naver.com>
2024-10-05 11:22:27 +0900

    추가: HttpClient 모듈 추가

--- campus/src/app/services/project/project.service.ts
+++ campus/src/app/services/project/project.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 
 
@@ -6,6 +7,8 @@ import { Injectable } from "@angular/core";
   })
 
   export class ProjectService {
+    private apiUrl = 'http://localhost:3000'; // 실제 API URL로 변경하세요.
 
+    constructor(private http: HttpClient) {}
 
   }
