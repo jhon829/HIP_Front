5c28f77837d0c4191a13ed1b9d388c8cae337029
jhk <qwertyjhk11@naver.com>
2024-10-05 12:17:33 +0900

    추가: projects service 코드 작성(CRUD)

--- campus/src/app/services/project/project.service.ts
+++ campus/src/app/services/project/project.service.ts
@@ -1,6 +1,10 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
+import { ApiResponse } from "src/app/models/common/api-response.interface";
+import { FeedbackResponseData } from "src/app/models/project/feedback/feedback-response.interface";
+import { ProjectDocResponseData } from "src/app/models/project/project_doc/project_doc-response.interface";
+import { ProjectResponseData } from "src/app/models/project/projects/projects-response.interface";
 
 
 @Injectable({
@@ -8,12 +12,25 @@ import { Observable } from "rxjs";
   })
 
   export class ProjectService {
-    private apiUrl = 'http://localhost:3000'; // 실제 API URL로 변경하세요.
+    private projectApiUrl = 'http://localhost:3000/projects'; // 실제 API URL로 변경하세요.
 
     constructor(private http: HttpClient) {}
-    
-    // createProject(): Observable<any> {
-    //     return this.http.post(`${this.apiUrl}/`);
-    //   }
+
+    // projects
+    createProject(projectData: any): Observable<ApiResponse<ProjectResponseData>> {
+        return this.http.post<ApiResponse<ProjectResponseData>>(`${this.projectApiUrl}/register`, projectData);
+    }
+
+    getAllProject(): Observable<ApiResponse<ProjectResponseData>> {
+        return this.http.get<ApiResponse<ProjectResponseData>>(`${this.projectApiUrl}`);
+    }
+
+    updateProject(id: number, projectData: any): Observable<ApiResponse<ProjectResponseData>> {
+        return this.http.patch<ApiResponse<ProjectResponseData>>(`${this.projectApiUrl}/${id}/update`, projectData);
+    }
       
+    deleteProject(id: number): Observable<ApiResponse<ProjectResponseData>> {
+        return this.http.delete<ApiResponse<ProjectResponseData>>(`${this.projectApiUrl}/${id}/delete`);
+    }
+
   }
