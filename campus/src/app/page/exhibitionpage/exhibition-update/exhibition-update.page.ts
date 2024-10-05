f63901da28ed1d927ab0f1e158336ab9724231ad
DongNyoen <qwert884@naver.com>
2024-10-05 16:05:33 +0900

    feat : exhibition-update페이지 추가

---
+++ campus/src/app/page/exhibitionpage/exhibition-update/exhibition-update.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-exhibition-update',
+  templateUrl: './exhibition-update.page.html',
+  styleUrls: ['./exhibition-update.page.scss'],
+})
+export class ExhibitionUpdatePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
