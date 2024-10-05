d37977599e7266f6413b7a7a3c84b83de848e04b
yongchaeyoung <happy7yong83@gmail.com>
2024-10-05 15:27:53 +0900

    doc : 수강신청 상태 정의

---
+++ campus/src/app/models/enums/role.enums.ts
@@ -0,0 +1,21 @@
+export enum Role {
+  STUDENT = 'student',
+  INSTRUCTOR = 'instructor',
+  ADMIN = 'admin',
+}
+
+export enum ProjectStatus {
+  IN_PROGRESS = 'in_progress',
+  COMPLETED = 'completed'
+}
+
+export enum Registration {
+  APPROVED = 'approved',
+  PENDING = 'pending',
+  REJECTED = 'rejected'
+}
+
+export enum TeamRole {
+  LEADER = 'leader',
+  MEMBER = 'member'
+}
