97288e383f484648db1a4b31e87dee35aaf50c06
yongchaeyoung <happy7yong83@gmail.com>
2024-10-12 11:32:43 +0900

    docs : courseTitle component 생성

---
+++ campus/src/app/component/course-title/course-title.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-course-title',
+  templateUrl: './course-title.component.html',
+  styleUrls: ['./course-title.component.scss'],
+})
+export class CourseTitleComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
