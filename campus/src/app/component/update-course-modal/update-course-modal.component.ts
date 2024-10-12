322b744e94218774ba87181378d4f9f09051ab12
yongchaeyoung <happy7yong83@gmail.com>
2024-10-12 14:26:58 +0900

    docs : updateCourse-modal 생성

---
+++ campus/src/app/component/update-course-modal/update-course-modal.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-update-course-modal',
+  templateUrl: './update-course-modal.component.html',
+  styleUrls: ['./update-course-modal.component.scss'],
+})
+export class UpdateCourseModalComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
