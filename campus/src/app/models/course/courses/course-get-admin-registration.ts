4f8eee8fdbd9f9fe5a2c858998dc41813b0e0ecb
yongchaeyoung <happy7yong83@gmail.com>
2024-11-05 03:28:39 +0900

    fix : dto add generation

--- campus/src/app/models/course/courses/course-get-admin-registration.ts
+++ campus/src/app/models/course/courses/course-get-admin-registration.ts
@@ -3,6 +3,7 @@ import { UseResponse } from '../../common/use-response';
 import { CourseResponseDto } from '../courses/course-response.interface'
 
 export interface AdminResponseCourseRegistrationDto {
+  generation: number;
   course_id: number;
   course_registration_status: Registration;
   course_reporting_date: Date;
--- campus/src/app/models/course/courses/course-registration.interface.ts
+++ campus/src/app/models/course/courses/course-registration.interface.ts
@@ -1,6 +1,7 @@
 import { Registration } from '../../enums/role.enums';
 
 export interface CreateCourseRegistrationDto {
+  generation:number;
   course_reporting_date: string;
   course_registration_status: Registration; // 강의 등록 상태 (열거형)
 }
