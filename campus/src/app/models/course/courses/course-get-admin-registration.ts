1885cdb59d7abe7cc9efa859f4f737d0fe52ac35
yongchaeyoung <happy7yong83@gmail.com>
2024-11-05 05:33:46 +0900

    fit : delete generation

--- campus/src/app/models/course/courses/course-get-admin-registration.ts
+++ campus/src/app/models/course/courses/course-get-admin-registration.ts
@@ -3,7 +3,7 @@ import { UseResponse } from '../../common/use-response';
 import { CourseResponseDto } from '../courses/course-response.interface'
 
 export interface AdminResponseCourseRegistrationDto {
-  generation: number;
+  //generation: number;
   course_id: number;
   course_registration_status: Registration;
   course_reporting_date: Date;
