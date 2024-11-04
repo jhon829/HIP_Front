3d354fac2d710c39eea909dd1f23ef3968d7fdfb
yongchaeyoung <happy7yong83@gmail.com>
2024-11-04 13:17:17 +0900

    feat : get-admin registration interface작성

--- campus/src/app/models/course/courses/course-registration.interface.ts
+++ campus/src/app/models/course/courses/course-registration.interface.ts
@@ -1,6 +1,21 @@
+// registration-status.interface.ts
 import { Registration } from '../../enums/role.enums';
 
-export interface CreateCourseRegistrationDto {
-  course_reporting_date: String;
-  course_registration_status: Registration; // 강의 등록 상태 (열거형)
+export interface AdminResponseCourseRegistrationDto {
+  course_registration_status: Registration;
+  course_reporting_date: Date;
+  applicant: ApplicantInfo;
+  currentCourse: CurrentCourseInfo;
+}
+
+export interface ApplicantInfo {
+  user_name: string;
+  id: string;
+  email: string;
+  user_role: string;
+}
+
+export interface CurrentCourseInfo {
+  course_id: string;
+  course_title: string;
 }
