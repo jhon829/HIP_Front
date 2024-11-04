fbc2a576cab536d5685371a96e28bb8f72703499
yongchaeyoung <happy7yong83@gmail.com>
2024-11-04 15:47:32 +0900

    fit : admin-registration 변수 추가 및 수정

--- campus/src/app/models/course/courses/course-get-admin-registration.ts
+++ campus/src/app/models/course/courses/course-get-admin-registration.ts
@@ -1,14 +1,11 @@
 import { Registration } from '../../enums/role.enums';
-import { UseResponse } from '../../common/use-response'; // UseResponse 경로에 맞게 수정
+import { UseResponse } from '../../common/use-response';
+import { CourseResponseDto } from '../courses/course-response.interface'
 
 export interface AdminResponseCourseRegistrationDto {
+  course_id: number;
   course_registration_status: Registration;
   course_reporting_date: Date;
-  applicant: UseResponse; // 기존 UseResponse 인터페이스 사용
-  currentCourse: CurrentCourseInfo;
-}
-
-export interface CurrentCourseInfo {
-  course_id: string;
-  course_title: string;
+  applicant: UseResponse;
+  currentCourse: CourseResponseDto;
 }
