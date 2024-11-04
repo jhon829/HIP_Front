59e577c363a5f224fe222cc50aeebffaad7a37dd
yongchaeyoung <happy7yong83@gmail.com>
2024-11-05 06:21:48 +0900

    fit : course_id 제거

--- campus/src/app/models/course/courses/course-get-admin-registration.ts
+++ campus/src/app/models/course/courses/course-get-admin-registration.ts
@@ -4,7 +4,7 @@ import { CourseResponseDto } from '../courses/course-response.interface'
 
 export interface AdminResponseCourseRegistrationDto {
   //generation: number;
-  course_id: number;
+  //course_id: number;
   course_registration_status: Registration;
   course_reporting_date: Date;
   applicant: UseResponse;
