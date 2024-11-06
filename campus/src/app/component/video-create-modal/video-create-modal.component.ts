96e3dfad1a46d4a6c6dd1f17e0b256b7f696707d
jhk <qwertyjhk11@naver.com>
2024-11-06 18:38:15 +0900

    생성: video-create-modal, 비디오 토픽에서 영상 생성

--- campus/src/app/component/video-create-modal/video-create-modal.component.ts
+++ campus/src/app/component/video-create-modal/video-create-modal.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { VideoService } from './video.service'; // 서비스 임포트
+
+interface Video {
+  file: File; // 파일 객체 추가
+  title: string;
+}
 
 @Component({
   selector: 'app-video-create-modal',
@@ -6,14 +12,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./video-create-modal.component.scss'],
 })
 export class VideoCreateModalComponent implements OnInit {
-  videos = [
-    { fileName: '영상파일.mp4', title: '영상 제목 1' },
-    { fileName: '영상파일.mp4', title: '영상 제목 2' },
-    { fileName: '영상파일.mp4', title: '영상 제목 3' },
-    { fileName: '영상파일.mp4', title: '영상 제목 4' },
-  ];
-
-  constructor() {}
+  videos: Video[] = [];
+  courseId: number = 0; // 초기값은 빈 문자열
+  videoTopicId: number = 0; // 초기값은 빈 문자열
+  
+  constructor(
+    private videoService: VideoService
+  ) {}
 
   ngOnInit() {}
+
+  onFileSelected(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files.length > 0) {
+      const file = input.files[0];
+      const video: Video = {
+        file: file, // 파일 객체를 저장
+        title: '영상 제목 ' + (this.videos.length + 1),
+      };
+      this.videos.push(video); // 비디오 목록에 추가
+      console.log(this.videos); // 콘솔에 현재 비디오 목록 출력
+    }
+  }
+
+  uploadVideo(video: Video) { // Video 타입으로 변경
+    this.videoService.uploadVideo(this.courseId, this.videoTopicId, video.file).subscribe(
+      response => {
+        console.log('업로드 성공:', response);
+      },
+      error => {
+        console.error('업로드 실패:', error);
+      }
+    );
+  }
+
+  onUpload() {
+    if (this.videos.length > 0) {
+      const video = this.videos[this.videos.length - 1]; // 마지막으로 추가된 비디오 객체
+      this.uploadVideo(video); // 파일 업로드
+    } else {
+      console.error('비디오 파일이 없습니다.');
+    }
+  }
 }
