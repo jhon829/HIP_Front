c0b99ce8ca7484465a18469d74436522cc4d1530
jhk <qwertyjhk11@naver.com>
2024-11-21 16:31:14 +0900

    추가: videoTitle 추가

--- campus/src/app/component/video-create-modal/video.service.ts
+++ campus/src/app/component/video-create-modal/video.service.ts
@@ -1,5 +1,6 @@
+// video.service.ts
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
@@ -7,13 +8,24 @@ import { Observable } from 'rxjs';
 })
 export class VideoService {
   private ApiUrl = 'http://localhost:3000/courses';
-  constructor(
-    private http: HttpClient
-  ) { }
 
-  uploadVideo(courseId: number, videoTopicId: number, file: File): Observable<any> {
+  constructor(private http: HttpClient) {}
+
+  private getAuthHeaders(): HttpHeaders {
+    const token = localStorage.getItem('token');
+    return new HttpHeaders().set('Authorization', `Bearer ${token}`);
+  }
+
+  uploadVideo(courseId: number, videoTitle: string, videoTopicId: number, file: File): Observable<any> {
     const formData = new FormData();
-    formData.append('file', file); // 비디오 파일 추가
-    return this.http.post(`${this.ApiUrl}/${courseId}/${videoTopicId}/video/upload`, formData);
+    formData.append('file', file);
+    formData.append('video_title', videoTitle);
+
+    const headers = this.getAuthHeaders();
+    return this.http.post(
+      `${this.ApiUrl}/${courseId}/${videoTopicId}/video/upload`,
+      formData,
+      { headers }
+    );
   }
 }
