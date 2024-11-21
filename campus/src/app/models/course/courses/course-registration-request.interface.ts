70add07b031a2ec6e659902f11f51be653f9bf56
jhk <qwertyjhk11@naver.com>
2024-11-21 09:39:34 +0900

    추가: CourseRegistrationRequestData 정의

---
+++ campus/src/app/models/course/courses/course-registration-request.interface.ts
@@ -0,0 +1,6 @@
+import { Registration } from "../../enums/role.enums";
+
+export interface CourseRegistrationRequestData {
+    course_registration_status: Registration;
+    course_reporting_date: Date;
+}
