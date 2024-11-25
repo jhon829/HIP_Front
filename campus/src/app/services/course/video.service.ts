7d966689be0552f455eda3a8d9c50cee4f44b01e
jhk <qwertyjhk11@naver.com>
2024-11-25 17:21:36 +0900

    추가: getFormDataAuthHeaders 메서드 추가

--- campus/src/app/services/course/video.service.ts
+++ campus/src/app/services/course/video.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { ApiResponse } from 'src/app/models/common/api-response.interface';
+import { VideoResponseData } from 'src/app/models/course/video/video-response.interface';
+import { VideoRequestData } from 'src/app/models/course/video/video-request.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -12,11 +14,6 @@ export class VideoService {
 
   constructor(private http: HttpClient) {}
 
-  // private getAuthHeaders(): HttpHeaders {
-  //   const token = localStorage.getItem('token');
-  //   return new HttpHeaders().set('Authorization', `Bearer ${token}`);
-  // }
-
   getAuthHeaders() {
     const token = localStorage.getItem('token'); // 또는 다른 저장소에서 토큰 가져오기
     return {
@@ -24,23 +21,35 @@ export class VideoService {
       'Content-Type': 'application/json'
     };
   }
+
+  getFormDataAuthHeaders() {
+    const token = localStorage.getItem('token'); // 또는 다른 저장소에서 토큰 가져오기
+    return {
+      Authorization: `Bearer ${token}`
+    };
+  }
+
+  // private getAuthHeaders(): HttpHeaders {
+  //   const token = localStorage.getItem('token');
+  //   return new HttpHeaders().set('Authorization', `Bearer ${token}`);
+  // }
   
-  uploadVideo(courseId: number, videoTitle: string, videoTopicId: number, file: File): Observable<any> {
+  uploadVideo(courseId: number, videoTitle: string, videoTopicId: number, file: File): Observable<ApiResponse<VideoRequestData>> {
     const formData = new FormData();
     formData.append('file', file);
     formData.append('video_title', videoTitle);
 
-    const headers = this.getAuthHeaders();
-    return this.http.post(
+    const headers = this.getFormDataAuthHeaders();
+    return this.http.post<ApiResponse<VideoRequestData>>(
       `${this.ApiUrl}/${courseId}/${videoTopicId}/video/upload`,
       formData,
       { headers }
     );
   }
 
-  // 영상 조회(스트리밍, GET) => 추가적으로 로직 작성 필요
-  streamVideo(courseId: number, videoTopicId: number, videoId: number): Observable<ApiResponse<{ url: string }>> {
+  // 영상 조회(PreSignedUrl GET)
+  streamVideo(courseId: number, videoTopicId: number, videoId: number): Observable<ApiResponse<VideoResponseData>> {
     const headers = this.getAuthHeaders();
-    return this.http.get<ApiResponse<{ url: string }>>(`${this.ApiUrl}/${courseId}/${videoTopicId}/video/${videoId}/stream`, { headers })
+    return this.http.get<ApiResponse<VideoResponseData>>(`${this.ApiUrl}/${courseId}/${videoTopicId}/video/${videoId}/stream`, { headers })
   }
 }
